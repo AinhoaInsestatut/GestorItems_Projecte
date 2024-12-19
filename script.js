21887666786ef3942d9166a67990da1e66083b0b
Rafi <rafaelrobles.alum@insestatut.cat>
2024-12-19 18:00:23 +0100

    Hay que simplificar items

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 class Gestor {
 
-    itemSimple = [];
-    itemVisual = [];
+    itemsSimples = [];
+    itemsVisuales = [];
 
     constructor(itemSimple, itemVisual) {
-        this.itemSimple = itemSimple;
+        this.itemsSimples = itemSimple;
         this.itemVisual = itemVisual;
     }
     
     llistarItem() {
-       if (this.itemSimple && this.itemVisual) {
-            console.log(this.itemSimple);
-            console.log(this.itemVisual);
+       if (this.itemsSimples && this.itemsVisuales) {
+            console.log(this.itemsSimples);
+            console.log(this.itemsVisuales);
         } else {
             console.log("No existe ningún item.");
         }
     }
 
     modificarItem(dataCreacio) {
-        
+        nomMod = document.getElementById("nombreNou")
+        descripcioMod = document.getElementById("descNou")
         dataModificacio = new Date()
         if () {
-            this.itemSimple. = {"nom" : nomMod, "descripció" : descripcioMod, "dataCreacio" : dataCreacio, "dataModificacio" : dataModificacio }
+            this.itemsSimples = {"nom" : nomMod, "descripció" : descripcioMod, "dataCreacio" : dataCreacio, "dataModificacio" : dataModificacio }
         }
     }
     crearItem() {
@@ -29,7 +30,7 @@ class Gestor {
         descripcio = document.getElementById("descripcio").textContent
         dataCreacio = new Date()
         if(this) {
-            this.itemSimple.push({"nom" : nom, "descripció" : descripcio, "dataCreacio" : dataCreacio, "dataModificacio" : dataCreacio })
+            this.itemsSimples.push({"nom" : nom, "descripció" : descripcio, "dataCreacio" : dataCreacio, "dataModificacio" : dataCreacio })
         }
     }
     
