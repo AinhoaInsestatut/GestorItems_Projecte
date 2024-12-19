8247efe7294f74d6f0868f0ab28ac269c3f76e67
AinhoaInsestatut <156360627+AinhoaInsestatut@users.noreply.github.com>
2024-12-19 17:21:39 +0100

    JS changes

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 class Gestor {
 
-    constructor(item) {
-        this.item = item;
+    itemSimple = [
+        {"nom" : "", "descripció" : "", "dataCreacio" : "", "dataModificacio" : "" },
+        {"nom" : "", "descripció" : "", "dataCreacio" : "", "dataModificacio" : "" },
+        
+    ]
+
+    itemVisual = [
+        {"nom" : "", "descripció" : "", "url" : "", "dataCreacio" : "", "dataModificacio" : "" },
+        {"nom" : "", "descripció" : "", "url" : "", "dataCreacio" : "", "dataModificacio" : "" },
+    
+    ]
+
+    constructor(itemSimple, itemVisual) {
+        this.itemSimple = itemSimple;
+        this.itemVisual = itemVisual;
     }
     
     llistarItem() {
-       if (this.item) {
-            console.log(this.item);
+       if (this.itemSimple && this.itemVisual) {
+            console.log(this.itemSimple);
+            console.log(this.itemVisual);
         } else {
             console.log("No existe ningún item.");
         }
+    }
+
+    crearItem() {
+
     }
 
     modificarItem() {
