40f005a0395f4666b82fd432e6af4efaf99fdd27
AinhoaInsestatut <156360627+AinhoaInsestatut@users.noreply.github.com>
2024-12-19 16:53:35 +0100

    Javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+class Gestor {
+
+    constructor(item) {
+        this.item = item;
+    }
+    
+    llistarItem() {
+       if (this.item) {
+            console.log(this.item);
+        } else {
+            console.log("No existe ningún item.");
+        }
+    }
+
+    modificarItem() {
+
+    }
+
+    eliminarItem() {
+
+    }
+
+
+}
