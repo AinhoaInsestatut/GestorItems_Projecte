117929960856acddd4716868ae795756bb37c04c
Rafi <rafaelrobles.alum@insestatut.cat>
2025-01-13 17:00:41 +0100

    a

--- items.js
+++ items.js
@@ -1,15 +1,19 @@
 class itemSimple{
+
     constructor(nom, descripcio, dataCreacio, dataUltModificacio){
         this.nom = nom;
         this.descripcio = descripcio;
         this.dataCreacio = dataCreacio;
         this.dataModificacio = dataUltModificacio;
     }
+
 }
 
 class itemVisual extends itemSimple{
+
     constructor(nom, descripcio, dataCreacio, dataUltModificacio, imatge){
         super(nom, descripcio, dataCreacio, dataUltModificacio);
         this.imatge = imatge;
     }
+    
 }
