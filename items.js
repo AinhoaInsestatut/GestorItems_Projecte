f93c6e82d72adec46b3f3a2221bc3eff3d913493
Rafi <rafaelrobles.alum@insestatut.cat>
2025-01-19 21:27:18 +0100

    Cambiamos item simple

--- items.js
+++ items.js
@@ -3,8 +3,8 @@ class itemSimple{
     constructor(nom, descripcio, dataCreacio, dataUltModificacio){
         this.nom = nom;
         this.descripcio = descripcio;
-        this.dataCreacio = dataCreacio;
-        this.dataModificacio = dataUltModificacio;
+        this.dataCreacio = new Date;
+        this.dataModificacio = this.dataCreacio;
     }
 
 }
