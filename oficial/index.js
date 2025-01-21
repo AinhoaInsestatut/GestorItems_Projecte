322cc85f86128830cd47fedf4aafa866fea9f311
Rafi <rafaelrobles.alum@insestatut.cat>
2025-01-21 19:57:58 +0100

    cambios

--- oficial/index.js
+++ oficial/index.js
@@ -3,23 +3,29 @@ class Item {
         this.nom = nom;
         this.descripcio = descripcio;
         this.url = url || null;
-        this.dataCreacio = new Date().toLocaleString();
+        this.dataCreacio = new Date();
         this.ultimaModificacio = this.dataCreacio;
     }
 }
 
 class GestorItems {
+
     constructor() {
+
         this.items = [];
         this.itemEditant = null; // Per identificar si estem editant un ítem
         this.mostraItems();
+
     }
 
     desaItems() {
+
         localStorage.setItem('items', JSON.stringify(this.items));
+
     }
 
     afegeixItem(item) {
+
         if (this.items.find(i => i.nom === item.nom)) {
             alert('Existeix un ítem amb aquest nom!');
             return;
@@ -27,20 +33,25 @@ class GestorItems {
         this.items.push(item);
         this.desaItems();
         this.mostraItems();
+
     }
 
     eliminaItem(nom) {
+
         this.items = this.items.filter(item => item.nom !== nom);
         this.desaItems();
         this.mostraItems();
+
     }
 
     mostraItems() {
-        const llistaItems = document.getElementById('llistatItems');
+
+        let llistaItems = document.getElementById('llistatItems');
         llistaItems.innerHTML = '';
 
         for (let item of this.items) {
-            const itemDiv = `
+
+            let itemDiv = `
                 <div class="item">
                     <strong>${item.nom}</strong><br>
                     <span>Creat: ${item.dataCreacio}</span><br>
@@ -55,15 +66,15 @@ class GestorItems {
 
     cercarItems(consulta) {
         consulta = consulta.toLowerCase();
-        const itemsFiltrats = this.items.filter(item =>
+        let itemsFiltrats = this.items.filter(item =>
             item.nom.toLowerCase().includes(consulta)
         );
 
-        const llistaItems = document.getElementById('llistatItems');
+        let llistaItems = document.getElementById('llistatItems');
         llistaItems.innerHTML = '';
 
         for (let item of itemsFiltrats) {
-            const itemDiv = `
+            let itemDiv = `
                 <div class="item">
                     <strong>${item.nom}</strong><br>
                     <span>Creat: ${item.dataCreacio}</span><br>
@@ -77,9 +88,10 @@ class GestorItems {
     }
 
     crearOEditarItem() {
-        const nom = document.getElementById('nom').value;
-        const descripcio = document.getElementById('descripcio').value;
-        const url = document.getElementById('imatge').value;
+
+        let nom = document.getElementById('nom').value;
+        let descripcio = document.getElementById('descripcio').value;
+        let url = document.getElementById('imatge').value;
 
         if (!nom) {
             alert('El nom és obligatori!');
@@ -87,9 +99,11 @@ class GestorItems {
         }
 
         if (this.itemEditant) {
+
             // Editar un ítem existent
-            const item = this.items.find(i => i.nom === this.itemEditant);
+            let item = this.items.find(i => i.nom === this.itemEditant);
             if (item) {
+
                 item.descripcio = descripcio;
                 item.url = url;
                 item.ultimaModificacio = new Date().toLocaleString();
@@ -97,29 +111,38 @@ class GestorItems {
                 this.mostraItems();
                 this.itemEditant = null;
                 this.netejaFormulari();
+
             }
+
         } else {
+
             // Crear un ítem nou
-            const nouItem = new Item(nom, descripcio, url);
+            let nouItem = new Item(nom, descripcio, url);
             this.afegeixItem(nouItem);
+
         }
     }
 
     editarItem(nom) {
-        const item = this.items.find(i => i.nom === nom);
+
+        let item = this.items.find(i => i.nom === nom);
         if (item) {
+
             this.itemEditant = nom; // Guardem el nom de l'ítem que s'està editant
             document.getElementById('nom').value = item.nom;
             document.getElementById('descripcio').value = item.descripcio;
             document.getElementById('imatge').value = item.url || '';
+            
         }
     }
 
     netejaFormulari() {
+
         document.getElementById('nom').value = '';
         document.getElementById('descripcio').value = '';
         document.getElementById('imatge').value = '';
+
     }
 }
 
-const gestor = new GestorItems();
+let gestor = new GestorItems();
